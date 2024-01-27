08aba1a5346dce0225bb82966a95ba6a6431cc85
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-01-27 14:47:58 -0800

    Worked on search format

--- src/Components/Navbar/Search/Search.jsx
+++ src/Components/Navbar/Search/Search.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import './Search.css'
-import pokemon_background from '../../../assets/pokemon_background.png'
-import pokemon_pin from '../../../assets/pokemon_pin.png'
+import React from "react";
+import "./Search.css";
+import pokemon_background from "../../../assets/pokemon_background.png";
+import pokemon_pin from "../../../assets/pokemon_pin.png";
+import search_icon from "../../../assets/search_icon.png";
 
 const Search = () => {
   return (
-    <div className='search'>
+    <div className="search">
       <div className="search-top">
         <h1>Online Pokédex</h1>
         <img src={pokemon_pin} alt="" />
       </div>
       <form action="">
-        <input id='search-input' type="text" placeholder='Search a Pokémon!'/>
-        <button className='search-button'>Search</button>
+        <input
+          className="search-input"
+          type="text"
+          maxlength="20"
+          placeholder="Search a Pokémon!"
+        />
+        <button className="search-button">
+          <img src={search_icon} alt="" />
+        </button>
       </form>
-      <img className='search-image' src={pokemon_background} alt="" />
+      <img className="search-image" src={pokemon_background} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
