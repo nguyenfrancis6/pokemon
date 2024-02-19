1eb5719f3b053cc212a0e784f0ed35f9bde196ac
Francis <nguyenfrancis6@gmail.com>
2024-02-19 15:37:39 -0800

    Loading state plus fetch pokemon stats from search input

--- src/Components/PokeStats/PokeStats.jsx
+++ src/Components/PokeStats/PokeStats.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import './PokeStats.css'
+import React from "react";
+import "./PokeStats.css";
 
-const PokeStats = () => {
+const PokeStats = ({ pokemon, loading }) => {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="container-left">
-        <img src="" alt="" />
+        <img src={pokemon.sprites.front_default} alt="" />
       </div>
       <div className="container-right">
-        <p>Name</p>
-        <p>Type</p>
-        <p>Abilities</p>
+        <p>Name: {pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</p>
+        <p>
+          Type: {pokemon.types[0].type.name[0].toUpperCase() +
+            pokemon.types[0].type.name.substring(1)}
+          {pokemon.types[1]
+            ? "/" +
+              pokemon.types[1].type.name[0].toUpperCase() +
+              pokemon.types[1].type.name.substring(1)
+            : null}
+        </p>
+        <p>Abilities: {pokemon.abilities.map((abilityArray) => abilityArray.ability.name + '/')}</p>
         <p>Stats</p>
         <ul>
-          <li>HP: </li>
-          <li>Atk: </li>
-          <li>Def: </li>
-          <li>SpAtk: </li>
-          <li>SpDef: </li>
-          <li>Spd: </li>
+          <li>{pokemon.stats[0].stat.name[0].toUpperCase() + pokemon.stats[0].stat.name.substring(1)}: {pokemon.stats[0].base_stat}</li>
+          <li>{pokemon.stats[1].stat.name[0].toUpperCase() + pokemon.stats[1].stat.name.substring(1)}: {pokemon.stats[1].base_stat}</li>
+          <li>{pokemon.stats[2].stat.name[0].toUpperCase() + pokemon.stats[2].stat.name.substring(1)}: {pokemon.stats[2].base_stat}</li>
+          <li>{pokemon.stats[3].stat.name[0].toUpperCase() + pokemon.stats[3].stat.name.substring(1)}: {pokemon.stats[3].base_stat}</li>
+          <li>{pokemon.stats[4].stat.name[0].toUpperCase() + pokemon.stats[4].stat.name.substring(1)}: {pokemon.stats[4].base_stat}</li>
+          <li>{pokemon.stats[5].stat.name[0].toUpperCase() + pokemon.stats[5].stat.name.substring(1)}: {pokemon.stats[5].base_stat}</li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PokeStats
+export default PokeStats;
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -5,30 +5,35 @@ import pokemon_pin from "../../assets/pokemon_pin.png";
 import search_icon from "../../assets/search_icon.png";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import PokeStats from "../PokeStats/PokeStats";
 
 const Search = () => {
-
-  const [searchTerm, setSearchTerm] = useState("")
-  const [pokemonData, setPokemonData] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const searchPokemon = async (pokemon) => {
     if (!pokemon) {
       return alert("Put in a Pokemon name or number");
     }
     try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    setPokemonData(response.data)
-    setErrorMessage('')
+      setLoading(true)
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      );
+      setPokemonData(response.data);
+      setLoading(false)
+      setErrorMessage("");
     } catch (error) {
-      console.error('An error occurred: ', error)
-      setErrorMessage("Pokemon doesn't exist or is not found!")
+      console.error("An error occurred: ", error);
+      setErrorMessage("Pokemon doesn't exist or is not found!");
     }
-  }
+  };
 
   useEffect(() => {
-    console.log(pokemonData)
-  }, [pokemonData])
+    console.log(pokemonData);
+  }, [pokemonData]);
 
   return (
     <div className="search">
@@ -46,11 +51,16 @@ const Search = () => {
           onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
         />
         <button className="search-button">
-          <img src={search_icon} alt="" onClick={() => searchPokemon(searchTerm)}/>
+          <img
+            src={search_icon}
+            alt=""
+            onClick={() => searchPokemon(searchTerm)}
+          />
         </button>
       </div>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
-      <img className="search-image" src={pokemon_background} alt="" />
+      {loading ? <img className="search-image" src={pokemon_background} alt="" /> : <PokeStats pokemon={pokemonData} loading={loading}/>}
+
     </div>
   );
 };
